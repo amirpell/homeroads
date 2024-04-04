a8db679ef92e67941a4f9d15e0bb5bc4af368328
amirpell <amirpb87@gmail.com>
2024-04-04 22:32:34 +0300

    amir

---
+++ screens/HomeProfile.js
@@ -0,0 +1,26 @@
+import { StyleSheet ,Text } from "react-native";
+import React from "react";
+
+const HomeProfile = () => {
+
+  
+  
+
+
+ 
+ 
+  return (
+
+   
+<Text>asdjasdjasd</Text>
+       
+
+  );
+};
+
+export default HomeProfile;
+
+const styles = StyleSheet.create({
+ 
+
+});
